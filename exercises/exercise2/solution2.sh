c2a618a181042e2baf91f2ba8b4fc422ffa84a29
gracesfinn <50175328+gracesfinn@users.noreply.github.com>
2019-10-06 19:37:03 -0500

    Update solution2.sh
    I'm unsure which is the MAC address for the dbserver?

--- exercises/exercise2/solution2.sh
+++ exercises/exercise2/solution2.sh
@@ -1,1 +1,5 @@
-# Write your script here!!! Hint: pehaps use pcap file name and MAC address as input arguements to the script...
+#!/bin/bash
+#This is a script which uses the webserver-eth1-trafffic2.pcap to confirm that ethernet frames were recieved from the 
+# dbserver using the MAC address.
+
+tshark -r /vagrant/webserver-eth1-traffic2.pcap -T fields -e ip -e eth | grep "08:00:27:d1:80:3b"
